b62e1bffde0708775ad1f7a5dfc587920c6068c5
Vranda Garg <gargvranda963@gmail.com>
2025-02-15 23:44:36 +0530

    file name was wrong in authController so fixed it

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
-const User = require("../models/userModel");
+const User = require("../models/User");
 const sendEmail = require("../utils/sendEmail"); // ✅ Reusable email function
 
 // ✅ Forgot Password (Generate Reset Token)
