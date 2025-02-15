a548ff47606e8ad07c374dcdd5cf980552c35f26
Vranda Garg <gargvranda963@gmail.com>
2025-02-15 17:17:52 +0530

    ...

--- Frontend/src/Pages/Game2048.jsx
+++ Frontend/src/Pages/Game2048.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../api";
-import LeaderboardButton from "../Components/Leaderboardbutton";
+import LeaderboardButton from "../Components/LeaderboardButton";
 
 const SIZE = 4;
 
